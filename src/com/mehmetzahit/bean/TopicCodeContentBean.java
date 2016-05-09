349de2f8b7095e26a3ddf08cf5e331ccdd0dd8c5
Mehmet Zahit Guney <mehmetzahitt@gmail.com>
2016-05-09 10:41:46 +0300

    no message

--- src/com/mehmetzahit/bean/TopicCodeContentBean.java
+++ src/com/mehmetzahit/bean/TopicCodeContentBean.java
@@ -7,6 +7,7 @@ import javax.faces.bean.SessionScoped;
 
 import org.hibernate.Session;
 
+import com.mehmetzahit.dao.TopicCodeContentDAO;
 import com.mehmetzahit.entities.TopicCodeContent;
 import com.mehmetzahit.entities.TopicContent;
 import com.mehmetzahit.util.HibernateUtil;
@@ -74,24 +75,8 @@ public class TopicCodeContentBean implements Serializable {
 	}
 
 	public String addCode() {
-		TopicCodeContent tcc=new TopicCodeContent();
 		
-		tcc.setCodeTitle(codeTitle);
-		tcc.setCodeContent(codeContent);
-		tcc.setContentID(contentID);
-		tcc.setTopicID(topicID);
-		tcc.setMemberID(memberID);
-		
-		Session session=HibernateUtil.getSessionfactory().openSession();
-		session.beginTransaction();
-		session.save(tcc);
-		session.getTransaction().commit();
-		
-		
-		
-		System.out.println("1:" + contentID + "2:" + getMemberID() + "3:" + topicID);
-
-		return "TopicContent?faces-redirec=true";
+		return TopicCodeContentDAO.SaveCode(codeTitle, codeContent, contentID, topicID, memberID);
 
 	}
 
--- src/com/mehmetzahit/dao/TopicCodeContentDAO.java
+++ src/com/mehmetzahit/dao/TopicCodeContentDAO.java
@@ -1,5 +1,36 @@
 package com.mehmetzahit.dao;
 
+import org.hibernate.Session;
+
+import com.mehmetzahit.entities.TopicCodeContent;
+import com.mehmetzahit.util.HibernateUtil;
+
 public class TopicCodeContentDAO {
+
+	public static String SaveCode(String codeTitle, String codeContent, int contentID, int topicID, int memberID) {
+
+		TopicCodeContent tcc = new TopicCodeContent();
+
+		tcc.setCodeTitle(codeTitle);
+		tcc.setCodeContent(codeContent);
+		tcc.setContentID(contentID);
+		tcc.setTopicID(topicID);
+		tcc.setMemberID(memberID);
+
+		try {
+			Session session = HibernateUtil.getSessionfactory().openSession();
+			session.beginTransaction();
+			session.save(tcc);
+			session.getTransaction().commit();
+
+		} catch (Exception e) {
+			System.out.println("Kayýt Ekleme hatasý");
+		}
+
+		System.out.println("1:" + contentID + "2:" + memberID + "3:" + topicID);
+
+		return "TopicContent?faces-redirec=true";
+
+	}
 
 }
